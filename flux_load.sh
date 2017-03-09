246acbe974ab7b0cca20010cf7a1bea1727146eb
Chun-Jung Chiu <b99501002@gmail.com>
2017-03-09 11:26:32 -0500

    Modify flux_load

--- flux_load.sh
+++ flux_load.sh
@@ -1,7 +1,11 @@
 module load python-dev/3.5.2
-module load cuda/7.5
 
-export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0rc0-cp35-cp35m-linux_x86_64.whl
+# Requirement for tensorflow 1.0
+module load cuda/8.0.27rc
+module load numpy-dev
+module load scipy-dev/0.17.1
+
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.0.1-cp35-cp35m-linux_x86_64.whl
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/sw/arcts/centos7/modulefiles/cuda/lib64"
 
 pip install --user --upgrade $TF_BINARY_URL
